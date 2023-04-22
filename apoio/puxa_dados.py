577888d228b93c62fdc0b2f198118512c4f110f0
YudiKonishiTempo <yudi.konishi@tempoenergia.com.br>
2023-04-22 20:12:52 -0300

    Puxando dados da tabela das salas e docentes

--- apoio/puxa_dados.py
+++ apoio/puxa_dados.py
@@ -3,10 +3,12 @@ import pandas as pd
 from io import StringIO
 
 class PopularBanco:
-    def __init__(self, arquivo):
-        self.df_turmas = self.formata_df(arquivo)
+    def __init__(self, caminho_turmas, caminho_salas):
+        self.df_turmas = self.formata_turmas(caminho_turmas)
+        self.df_salas = self.formata_salas(caminho_salas)
+        print(self.df_salas)
 
-    def formata_df(self, arquivo):
+    def formata_turmas(self, arquivo):
         turmas = pd.read_excel(arquivo)
         #definir o nome das colunas: 'RA' 'CÓDIGO TURMA' 'TURMA'
         turmas.columns = turmas.iloc[2]
@@ -15,6 +17,15 @@ class PopularBanco:
         turmas = turmas.reset_index(drop=True)
         return turmas
     
+    def formata_salas(self, arquivo):
+        salas = pd.read_excel(arquivo)
+        salas.columns = salas.iloc[44]
+        salas = salas.iloc[45:, 0:12]
+        #dropar linhas que tenham mesmo valor do cabeçalho
+        salas = salas.drop(salas[salas['CURSO'] == 'CURSO'].index)
+        salas = salas.reset_index(drop=True)
+        return salas
+
     def puxa_turmas(self):
         turmas = self.df_turmas
         # Exibe o resultado
@@ -22,9 +33,11 @@ class PopularBanco:
         return minhas_matriculas
 
 def main():
-    arquivo = 'apoio/arquivos/ajuste_2023_1_deferidos_pos_ajuste .xlsx'
-    puxa_dados = PopularBanco(arquivo)
-    puxa_dados.puxa_turmas()
+    base_path = 'apoio/arquivos/'
+    turmas = base_path + 'ajuste_2023_1_deferidos_pos_ajuste .xlsx'
+    salas = base_path + 'turmas_salas_docentes_2023_1.xlsx'
+    puxa_dados = PopularBanco(turmas, salas)
+    # puxa_dados.puxa_turmas()
     
 if __name__ == '__main__':
     main()
