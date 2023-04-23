b8776503c0858c9ac37b56ee7e824919e12a0a20
YudiKonishiTempo <yudi.konishi@tempoenergia.com.br>
2023-04-23 16:53:56 -0300

    feat: Criar script para popular banco

--- apoio/puxa_dados.py
+++ apoio/puxa_dados.py
@@ -1,12 +1,11 @@
-import PyPDF2 
+from django.core.exceptions import ObjectDoesNotExist
+from app.models import Professor, Materia, RelacaoMateriaHorarioProfessor, RelacaoAlunoMateria
 import pandas as pd
-from io import StringIO
 
 class PopularBanco:
     def __init__(self, caminho_turmas, caminho_salas):
         self.df_turmas = self.formata_turmas(caminho_turmas)
         self.df_salas = self.formata_salas(caminho_salas)
-        print(self.df_salas)
 
     def formata_turmas(self, arquivo):
         turmas = pd.read_excel(arquivo)
@@ -26,17 +25,70 @@ class PopularBanco:
         salas = salas.reset_index(drop=True)
         return salas
 
-    def puxa_turmas(self):
+    def puxa_turmas(self, ra: int):
         turmas = self.df_turmas
         # Exibe o resultado
-        minhas_matriculas = turmas[turmas['RA'] == 11201922142]
+        minhas_matriculas = turmas[turmas['RA'] == ra]
         return minhas_matriculas
+
+    def popular_banco(self):
+        # Popula a tabela Professor
+        for professor in self.df_salas['PROFESSOR'].unique():
+            try:
+                professor_obj = Professor.objects.get(nome=professor)
+            except ObjectDoesNotExist:
+                professor_obj = Professor(nome=professor)
+                professor_obj.save()
+
+        dict_campus = {'SA': 'Santo André', 'SB': 'São Bernardo do Campo'}
+        # Popula a tabela Materia
+        for turma in self.df_turmas['CÓDIGO TURMA'].unique():
+            materia_info = self.df_turmas[self.df_turmas['CÓDIGO TURMA'] == turma].iloc[0]
+            turma = turma.strip()
+            try:
+                materia_obj = Materia.objects.get(codigo_turma=turma)
+            except ObjectDoesNotExist:
+                
+                materia_obj = Materia(codigo_turma=turma, turma=materia_info['TURMA'], campus=dict_campus[turma[-2:]])
+                materia_obj.save()
+
+        # Popula a tabela RelacaoMateriaHorarioProfessor
+        for _, row in self.df_salas.iterrows():
+            try:
+                materia_obj = Materia.objects.get(codigo_turma=row['CÓDIGO TURMA'])
+                professor_obj = Professor.objects.get(nome=row['PROFESSOR'])
+                #Adicionar lógica que pega o campo 'TEORIA' ou 'PRÁTICA' e  transforma 
+                relacao_obj = RelacaoMateriaHorarioProfessor(codigo_turma=materia_obj, id_professor=professor_obj, dia_semana=row['DIA'], horario=row['HORÁRIO'], sala=row['SALA'], tipo_semanal=row['TIPO'])
+                relacao_obj.save()
+            except ObjectDoesNotExist:
+                pass
+
+        # Popula a tabela RelacaoAlunoMateria
+        for _, row in self.df_turmas.iterrows():
+            # Cria ou pega o professor a partir do nome
+            professor, created = Professor.objects.get_or_create(nome=row['PROFESSOR'])
+            # Cria ou pega a materia a partir do codigo_turma
+            materia, created = Materia.objects.get_or_create(codigo_turma=row['CÓDIGO TURMA'], turma=row['TURMA'],
+                                                                campus=row['CAMPUS'], turno=row['TURNO'])
+            # Cria o objeto de RelacaoMateriaHorarioProfessor
+            RelacaoMateriaHorarioProfessor.objects.create(codigo_turma=materia, id_professor=professor,
+                                                            dia_semana=row['DIA'], horario=row['HORARIO'], sala=row['SALA'],
+                                                            tipo_semanal=row['TIPO'])
+            # Cria o objeto de RelacaoAlunoMateria
+            RelacaoAlunoMateria.objects.create(codigo_turma=materia, ra=row['RA'])
+
+
 
 def main():
     base_path = 'apoio/arquivos/'
     turmas = base_path + 'ajuste_2023_1_deferidos_pos_ajuste .xlsx'
     salas = base_path + 'turmas_salas_docentes_2023_1.xlsx'
     puxa_dados = PopularBanco(turmas, salas)
+    print(puxa_dados.df_turmas.columns)
+    # variacoes_teoria = puxa_dados.df_salas['TEORIA'].unique()
+    # variacoes_pratica = puxa_dados.df_salas['PRÁTICA'].unique()
+    # print(variacoes_teoria)
+    # print(variacoes_pratica)
     # puxa_dados.puxa_turmas()
     
 if __name__ == '__main__':
--- app/models.py
+++ app/models.py
@@ -1,20 +1,41 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-class Aluno(models.Model):
-	ra = models.IntegerField(primary_key=True)
-	nome = models.CharField(max_length=255)
-	
+
+# class Professor(models.Model):
+# 	nome = models.CharField(primary_key=True, max_length=255)
+
+# class Disciplina(models.Model):
+#      codigo = models.CharField(primary_key=True, max_length=255)
+# class Turma(models.Model):
+#     codigo = models.CharField(primary_key=True, max_length=255)
+#     campus = models.CharField(max_length=255)
+#     horarios_salas = models.CharField(max_length=255)
+#     professor = models.ForeignKey(Professor)
+#     is_teoria = models.BooleanField(default=True)
+
+# class AlunoTurma(models.Model):
+#     aluno = models.IntergerField()
+#     turma = models.ForeignKey(Turma)
+#     class Meta:
+#         unique_together = ('aluno', 'turma')
+
 class Professor(models.Model):
-	id = models.AutoField(primary_key=True)
-	nome = models.CharField(max_length=255)
-	
-class Disciplina(models.Model):
-	codigo = models.CharField(primary_key=True)
-	nome = models.CharField(max_length=255)
-	
-class Turma(models.Model):
-	id = models.AutoField(primary_key=True)
-	disciplina = models.Foreingkey(Disciplina)
-	professor = models.Foreingkey(Professor)
-	quadrimestre = models.IntegerFields()
-	ano = models.IntegerFields()
+    nome = models.CharField(primary_key=True, max_length=255)
+
+class Materia(models.Model):
+    codigo_turma = models.CharField(primary_key=True, max_length=255)
+    turma = models.CharField(max_length=255)
+    campus = models.CharField(max_length=255)
+
+class RelacaoMateriaHorarioProfessor(models.Model):
+    codigo_turma = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    id_professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    dia_semana = models.CharField(max_length=255)
+    horario = models.CharField(max_length=255)
+    sala = models.CharField(max_length=255)
+    tipo_semanal = models.CharField(max_length=255)
+
+class RelacaoAlunoMateria(models.Model):
+    codigo_turma = models.ForeignKey(Materia, on_delete=models.CASCADE)
+    ra = models.CharField(max_length=255)
